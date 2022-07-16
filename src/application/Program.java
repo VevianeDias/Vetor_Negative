e316a51275a0dde85291e4e7778a5da85b6904e4
VevianeDias <veviane_md@hotmail.com>
2022-07-16 00:33:35 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,33 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] vect = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			vect[i] = sc.nextInt();
+		}
+		
+		
+		System.out.println("NUMEROS NEGATIVOS:");
+
+	    for (int i=0; i<n; i++) {
+	        if (vect[i] < 0) {
+	        	System.out.printf("%d\n", vect[i]);
+	        }
+	    }
+		
+		sc.close();
+	}
+
+
+}
